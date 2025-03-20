d5e73053d077da4135b90d2a7dbd7774c3cbea5a
Rana Ahmad Ali <ahmedali1492027@gmail.com>
2025-03-21 01:06:45 +0500

    New

---
+++ ass.py
@@ -0,0 +1,14 @@
+#movies =  ["Chal mera put" , "Avengers", "The Boys" , "My Fault", "365 Days"]
+#for movie in movies:
+   # print(movies.index(movie),  movie)
+full_name="Ahmad Manzoor Wowsssss" 
+vowels="aeiouAEIOU"
+vowels_count =0;
+for char in full_name:
+    print(char)
+    if char in vowels:
+      vowels_count +=1
+     
+print("char:",vowels_count)
+
+
