9640f8681b44e630875e28b790002bc258527492
123AhmadAli <ahmedali1492027@gmail.com>
2025-03-22 21:00:29 +0500

    changes

--- ass.py
+++ ass.py
@@ -1,7 +1,12 @@
-#movies =  ["Chal mera put" , "Avengers", "The Boys" , "My Fault", "365 Days"]
-#for movie in movies:
-   # print(movies.index(movie),  movie)
-full_name="Ahmad Manzoor Wowsssss" 
+# question -01
+
+
+movies =  ["Chal mera put" , "Avengers", "The Boys" , "My Fault", "365 Days"]
+for movie in movies:
+    print(movies.index(movie),  movie)
+
+    # question - 02
+full_name="Ahmad Manzoor " 
 vowels="aeiouAEIOU"
 vowels_count =0;
 for char in full_name:
@@ -10,6 +15,6 @@ for char in full_name:
       vowels_count +=1
      
 print("char:",vowels_count)
-# Hello
+
 
 
