08d431592d9c30b58f9225d6d545089923038eb2
123AhmadAli <ahmedali1492027@gmail.com>
2025-03-27 14:23:57 +0500

    changes

---
+++ if.py
@@ -0,0 +1,57 @@
+# question no 01
+
+
+number=-5
+if number>=0:
+    print("the number is positive")
+elif number<=0:
+    print("the number is nagitive")
+elif number==0:
+    print("the number is zero")    
+
+
+    # question no 02 
+
+year_days =366
+if year_days==366:
+    print("this is a leap year")
+else:
+    print("this is not leap year")    
+
+    # question no 03
+
+number_1= 7
+number_2= 8
+operator ="+"
+if operator=="+":
+    resul= number_1+number_2
+    print(resul)
+elif operator=="-" :
+    resul=number_1-number_2
+    print(resul)
+elif operator=="*":
+    resul=number_1*number_2
+    print(resul)
+elif operator=="/":
+    resul=number_1/number_2 
+    print(resul)        
+else:
+    print("invalid operator")   
+     
+
+     # question no 4
+
+student_marks=90
+if student_marks>=85:
+    print("great A")
+elif student_marks>=70:
+    print(" Great B")    
+elif student_marks>=50:
+    print("great C")
+else:
+    print("FAIL") 
+
+
+
+
+
